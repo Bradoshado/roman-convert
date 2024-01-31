f809a0d0679b81648bf9bd836312b49c43f53aa5
Bradoshado <bradoshado@gmail.com>
2024-01-31 12:13:27 +0900

    added roman num convert func

--- script.js
+++ script.js
@@ -2,21 +2,43 @@ const convertBtn = document.getElementById("convert-btn");
 
 const convertToRoman = () => {
   const numberInput = document.getElementById("number").value;
-  const output = document.getElementById("output");
-
-  if (numberInput === "") {
-    output.innerText = "Please enter a valid number";
-    return;
-  }
-
+  let displayOutput = document.getElementById("output");
   const num = parseInt(numberInput);
 
-  if (isNaN(num) || num < 1 || num > 3999) {
-    output.innerText = "please enter a number between 1 and 3999";
+  if (isNaN(num)) {
+    displayOutput.innerText = "Please enter a valid number";
+    return;
+  }
+  if (num < 1) {
+    displayOutput.innerText =
+      "Please enter a number greater than or equal to 1";
+    return;
+  }
+  if (num > 3999) {
+    displayOutput.innerText =
+      "Please enter a number less than or equal to 3999";
     return;
   }
 
-  // Number stuff
+  const romanNumerals = [
+    ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
+    ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],
+    ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"],
+    ["", "M", "MM", "MMM"],
+  ];
+
+  const thousands = Math.floor(num / 1000);
+  const hundreds = Math.floor((num % 1000) / 100);
+  const tens = Math.floor((num % 100) / 10);
+  const ones = num % 10;
+
+  const result =
+    romanNumerals[3][thousands] +
+    romanNumerals[2][hundreds] +
+    romanNumerals[1][tens] +
+    romanNumerals[0][ones];
+
+  displayOutput.innerText = result;
 };
 
 convertBtn.addEventListener("click", convertToRoman);
